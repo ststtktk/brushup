80d2c2cac9ec3054bf0ba5b0f463288e4fca40fa
ststtktk <stst.tktk.5@icloud.com>
2022-02-05 13:45:14 +0900

    App change スペルミス

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,10 +25,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-        Schema::defaultStringLength(191);
-        if(\App::environment(['production'])){
-            \URL::forseScheme('https');
+        if (\App::environment(['production'])) {
+            \URL::forceScheme('https');
         }
-
     }
 }
