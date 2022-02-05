9a53335d12400224776a3d42694d38dcdeb70ed1
ststtktk <stst.tktk.5@icloud.com>
2022-02-05 14:24:58 +0900

    Scheme2設定

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,7 +25,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-        Scheme::defalutStringLength(191);
+        Scheme::defaultStringLength(191);
 
         if (\App::environment(['production'])) {
             \URL::forceScheme('https');
