292320361013f50fbd1806bccf025e9d05a5a8c9
ststtktk <stst.tktk.5@icloud.com>
2022-02-05 13:54:56 +0900

    charsetの変更

--- database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+++ database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -16,9 +16,9 @@ class CreatePersonalAccessTokensTable extends Migration
         Schema::create('personal_access_tokens', function (Blueprint $table) {
             $table->id();
             $table->morphs('tokenable');
-            $table->string('name')->charset("utf8");
+            $table->string('name');
             $table->string('token', 64)->unique();
-            $table->text('abilities')->charset("utf8")->nullable();
+            $table->text('abilities')->nullable();
             $table->timestamp('last_used_at')->nullable();
             $table->timestamps();
         });
